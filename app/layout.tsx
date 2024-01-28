04f2a2bd3b189a4f27e3fd5527bd2011778b0887
kenneth luzolo <kenneth.nepsther@gmail.com>
2024-01-28 16:19:43 +0100

    :lipstick: feat: customise font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter, Outfit } from 'next/font/google'
 import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
 
-const inter = Inter({ subsets: ['latin'] })
+const outfit = Outfit({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <ClerkProvider >
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={outfit.className}>{children}</body>
       </html>
     </ClerkProvider>
   )
