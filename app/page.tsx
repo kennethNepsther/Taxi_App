cbe7a236e32502f125733f16f08f931d9a669521
kenneth luzolo <kenneth.nepsther@gmail.com>
2024-02-02 06:49:47 +0100

    :lipstick: feat: customise initial layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import { UserButton } from '@clerk/nextjs'
-
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>             
-      <div>App de mobildade urbana</div>
+    <div>
+      <div className="grid grid-cols-1 md:grid-cols-3">
+        <div className="bg-blue-100">Booking</div>
+        <div className="col-span-2 bg-red-100 order-first md:order-last">Map</div>
+      </div>
     </div>
-  )
+  );
 }
